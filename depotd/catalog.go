60e28c3ca4f9c6917a2f68caadd5292dfcd23987
Till Wegmüller <toasterson@gmail.com>
2018-03-25 22:05:01 +0200

    Added: update file output to depot

--- depotd/catalog.go
+++ depotd/catalog.go
@@ -3,6 +3,7 @@ package depotd
 import (
 	"fmt"
 	"github.com/labstack/echo"
+	"strings"
 )
 
 func (d *DepotServer) handleCatalogV0(c echo.Context) error {
@@ -17,9 +18,13 @@ func (d *DepotServer) handleCatalogV1(c echo.Context) error {
 	var ok bool
 	if catalogPart == "catalog.attrs" {
 		return c.JSON(200, cat)
+	} else if strings.Contains(catalogPart, "update") {
+		if content, ok = cat.V1UpdateContent[catalogPart]; !ok {
+			return c.JSON(404, fmt.Errorf("update file %s does not exist", catalogPart))
+		}
 	} else {
 		if content, ok = cat.V1PartContent[catalogPart]; !ok {
-			return c.JSON(404, fmt.Errorf("file %s does not exist", catalogPart))
+			return c.JSON(404, fmt.Errorf("part file %s does not exist", catalogPart))
 		}
 	}
 	return c.JSON(200, content)
